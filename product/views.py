230815e3115a4d416bd7f8230fed38780c1b61f8
Hamad <hamad.sa.ghosen@gmail.com>
2024-09-05 11:50:06 +0300

    Add paginator to some pages

--- product/views.py
+++ product/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import Product ,ProductImage
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def product_list(request):
     product_list=Product.objects.all()
+    paginator=Paginator(product_list,2)
+    
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
     context={
-        'product_list':product_list
+        'product_list':page_obj
         }
     
     return render(request,'product/product_list.html',context)
@@ -15,9 +20,15 @@ def product_list(request):
 def product_details(request,id):
     product_details=Product.objects.get(id=id)
     product_images=ProductImage.objects.filter(PRDIProduct=id)
+    
+    paginator=Paginator(product_images,1)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    
     context={
         'product_details':product_details,
-        'product_images':product_images
+        # 'product_images':product_images,
+        'product_images':page_obj,
         }
     
     return render(request,'product/product_details.html',context)
